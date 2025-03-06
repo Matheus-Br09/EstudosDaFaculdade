9321b07302b98e5b227ca497aa94456792ada6b9
Matheus-Br09 <matheusps3.br06@gmail.com>
2025-03-06 09:50:02 -0300

    aula8

---
+++ Aulas_Casa/ex008.py
@@ -0,0 +1,11 @@
+#Aula sobre módulos
+#import random
+
+#num = random.randint(1, 10)
+#print(num)
+
+#from math import sqrt
+
+#num = int(input('Digite um número: '))
+#raiz = sqrt(num)
+#print(f'A raiz de {num} é: {raiz: .2f}')
